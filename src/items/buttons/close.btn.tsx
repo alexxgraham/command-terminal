e68370cf8164bebfbc12df2f53af916b2a70e352
Alex Graham <destgraham@icloud.com>
2024-06-12 20:16:25 -0700

    stage: (1d) -- custom stylesheets. "prefix issues"

--- src/items/buttons/close.btn.tsx
+++ src/items/buttons/close.btn.tsx
@@ -3,7 +3,7 @@ import { DispatchAction } from '@ag108/ct-utils/types/dispatch';
 
 export const CloseButton = ({ setOpen }: { setOpen: DispatchAction<boolean> }) => {
 	return (
-		<button onClick={() => setOpen(false)} className={cn('ct-utils_outline-none', 'ct-utils_hover text-destructive')}>
+		<button onClick={() => setOpen(false)} className={cn('ct-utils_outline-none', 'ct-utils_hover ct-utils_text-destructive')}>
 			X
 		</button>
 	);
--- src/items/misc/title.tsx
+++ src/items/misc/title.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@ag108/ct-utils';
 
 export const PopupTitle = ({ title, pad }: { title: string; pad?: boolean }) => {
-	const titleClass = cn(pad ? ['ct-utils_pad-x-10', 'ct-utils_hover underline', 'ct-utils_underline-offset-8'] : ['ct-utils_hover underline', 'ct-utils_underline-offset-8']);
+	const titleClass = cn(pad ? ['ct-utils_pad-x-10', 'ct-utils_hover ct-utils_underline', 'ct-utils_underline-offset-8'] : ['ct-utils_hover ct-utils_underline', 'ct-utils_underline-offset-8']);
 	return <p className={titleClass}>{title}</p>;
 };
