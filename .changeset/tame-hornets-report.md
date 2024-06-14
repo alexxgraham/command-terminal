4d6f030cad700ed39745bb8a703cb97f8660211b
Alex Graham <destgraham@icloud.com>
2024-06-13 23:15:50 -0700

    new: "terminal theme classes @ct-utils"

---
+++ .changeset/tame-hornets-report.md
@@ -0,0 +1,5 @@
+---
+"@ag108/command-terminal": patch
+---
+
+set terminal theme to a1
--- src/terminal.tsx
+++ src/terminal.tsx
@@ -3,7 +3,7 @@ import { CommandMenu, ProfileHub, SearchBar } from './items/_main.itm';
 
 export const Terminal = () => {
 	return (
-		<section className={cn('ct-utils_shrink-0', 'ct-utils_bg-secondary', 'ct-utils_w-15rem', 'ct-utils_h-3rem', 'ct-utils_rounded-3xl', 'ct-utils_flex', 'ct-utils_justify-center', 'ct-utils_items-center', 'ct-utils_gap-6')}>
+		<section className={cn('ct-utils_terminal-a1', 'ct-utils_shrink-0', 'ct-utils_bg-secondary', 'ct-utils_w-15rem', 'ct-utils_h-3rem', 'ct-utils_rounded-3xl', 'ct-utils_flex', 'ct-utils_justify-center', 'ct-utils_items-center', 'ct-utils_gap-6')}>
 			<CommandMenu />
 			<ProfileHub />
 			<SearchBar />
